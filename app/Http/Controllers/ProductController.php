b08134734921854088b69b04a39b82bba0f7f7c3
Curso Git <marcooperdomo@gmail.com>
2019-07-31 11:38:13 -0500

    Listar y Paginar elementos desde BD

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,10 +3,13 @@
 namespace CRUDLaravel54\Http\Controllers;
 
 use Illuminate\Http\Request;
+// Importado CRUDLaravel5.4
+use CRUDLaravel54\Product;
 
 class ProductController extends Controller
 {
     public function index(){
-        return view('products.index');
+        $products = Product::orderBy('id', 'Desc')->paginate();
+        return view('products.index', compact('products'));
     }
 }
