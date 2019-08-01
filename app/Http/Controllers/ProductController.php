8fd2983c7a651f1aedf09dfb779be7ed8c74496a
Curso Git <marcooperdomo@gmail.com>
2019-08-01 10:21:37 -0500

    Validación con Form Requests

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace CRUDLaravel54\Http\Controllers;
 use Illuminate\Http\Request;
 // Importado CRUDLaravel5.4
 use CRUDLaravel54\Product;
+use CRUDLaravel54\Http\Requests\ProductRequest;
 
 class ProductController extends Controller
 {
@@ -12,15 +13,23 @@ class ProductController extends Controller
         $products = Product::orderBy('id', 'Desc')->paginate();
         return view('products.index', compact('products'));
     }
+    
+    
+    public function create(){
+        return view('products.create');
+    }
 
+    public function store(ProductRequest $request){
+        return 'producto guardado';
+    }
 
     public function edit($id){
         $product = Product::find($id);
         return view('products.edit', compact('product'));
     }
 
-    public function create(){
-        return view('products.create');
+    public function update(ProductRequest $request, $id){
+        return 'producto actualizado' . $id;
     }
 
 
@@ -30,7 +39,6 @@ class ProductController extends Controller
     }
 
     
-
     public function destroy($id){
         $product = Product::find($id);
         $product->delete();
